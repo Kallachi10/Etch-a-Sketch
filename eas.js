e086d7e4e1ac2324052b70e2eda91a678d723321
johnsaju <jsajukallachiyil@gmail.com>
2024-07-16 18:22:15 +0530

    completed the basic structure

---
+++ eas.js
@@ -0,0 +1,37 @@
+const sketchPad = document.querySelector(".sketchPad");
+const slider = document.querySelector(".slider");
+const sliderValue = document.querySelector(".sliderValue");
+
+function handleSlide(event){
+    sliderValue.textContent = (event.target.value*16+16);
+    setSketchPad();
+}
+
+function changeColor(node){
+    node.target.style.backgroundColor="black";
+    node.target.removeEventListener("mouseenter",changeColor);
+}
+
+slider.addEventListener("input",handleSlide);
+
+function setSketchPad(){
+    sketchPad.innerHTML="";
+     limit = parseInt(sliderValue.textContent);
+    for(let i=0;i<limit;i++){
+
+        const row = document.createElement("div");
+        row.classList.add("rows");
+        
+        sketchPad.appendChild(row);
+        for(let j=0;j<limit;j++){
+            const cell = document.createElement("div");
+            cell.classList.add("cells");
+            cell.addEventListener("mouseenter",changeColor);
+            row.appendChild(cell);
+            
+        }
+        
+    }
+}
+sliderValue.textContent=16;
+setSketchPad();
